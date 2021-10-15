cd8683b71cf2cf11adc8ae78ea5c5c6308eb601f
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-15 14:42:12 -0400

    changing routes in products to show products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Sequelize } = require('sequelize/types');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
@@ -7,18 +8,14 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  try {
-    const productData = await Product.findAll();
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+})
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  // include: category and product tag
+  
 });
 
 // create new product
@@ -95,8 +92,12 @@ router.put('/:id', (req, res) => {
     });
 });
 
+
+
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
 });
+
+
 
 module.exports = router;
