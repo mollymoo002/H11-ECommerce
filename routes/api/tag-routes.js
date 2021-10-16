ded758880cfc6269f455d1b5ce1804149b41204c
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-16 00:35:00 -0400

    fixed an error on this path

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,9 +18,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk(req.params.id, {
-      include: [{model: Product}],
-    })
+    const tagData = await Tag.findByPk(req.params.id)
 
     if (!tagData) {
       res.status(404).json({ message: 'No category found with this id!' });
